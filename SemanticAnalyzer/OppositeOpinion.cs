dc3a1895fc23aa207f26206f1a2959402a826eef
MIDDLEEAST\adigerzi <adigerzi@microsoft.com>
2017-07-25 15:23:23 +0300

    no message

---
+++ SemanticAnalyzer/OppositeOpinion.cs
@@ -0,0 +1,19 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="OppositeOpinion.cs" company="Microsoft">
+//   Copyright (c) Microsoft Corporation. All rights reserved.
+// </copyright>
+// <summary>
+//   The opposite opinion.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace SemanticAnalyzer
+{
+    /// <summary>
+    /// The opposite opinion.
+    /// </summary>
+    public class OppositeOpinion
+    {
+        
+    }
+}
