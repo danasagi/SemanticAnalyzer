459be478c7d9302b4f1f64ee228526fb6bb64c0b
dasagi <dasagi@microsoft.com>
2017-07-25 16:26:03 +0300

    controller is ready fro url

--- SemanticAnalyzer/Program.cs
+++ SemanticAnalyzer/Program.cs
@@ -7,6 +7,12 @@
 // </summary>
 // --------------------------------------------------------------------------------------------------------------------
 
+using System;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json;
+using RestSharp;
+
 namespace SemanticAnalyzer
 {
     /// <summary>
@@ -21,11 +27,28 @@ namespace SemanticAnalyzer
         {
             // SandBox.GetEntitiesByText(Consts.t);
             // SandBox.GetSentimentsByText(Consts.text);
-
             /**
              *this is going to be the main method: 
              */
-            MeaningCloud.AnalyzeArticle(Consts.t);
+            //  MeaningCloud.AnalyzeArticle(Consts.t);
+            var url =
+            "https://www.nytimes.com/2017/07/25/us/politics/trump-attacks-own-attorney-general-jeff-sessions.html";
+            var textTitleSite = GetArticleData(url);
+        }
+
+        private static ArticleData GetArticleData(string url)
+        {
+
+            var requestContent = "https://api.diffbot.com/v3/article?" +
+                "token=642d8c5e2e5f11ca47e3128716bea5dc&url=" +
+                url +
+                "&fields=title,text,siteName";
+            var request = (HttpWebRequest)WebRequest.Create(requestContent);
+            var response = (HttpWebResponse)request.GetResponse();
+            var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+            var result = JsonConvert.DeserializeObject<dynamic>(responseString);
+            var objects = result.objects[0];
+            return new ArticleData(objects.title.ToString(), objects.text.ToString(), objects.siteName.ToString());
         }
     }
 }
--- SemanticAnalyzer/TopicOrientationsController.cs
+++ SemanticAnalyzer/TopicOrientationsController.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web.Http;
+using Newtonsoft.Json;
+using RestSharp;
 
 namespace SemanticAnalyzer
 {
@@ -17,10 +21,7 @@ namespace SemanticAnalyzer
             {
                 return null;
             }
-            string articleText = GetArticleText(url);
-            NewsPaper paperName = GetNewsPaper();
-            
-            
+            var articleData = GetArticleData(url);
             /**
              * need to remove the comment and the throw declaration when the AnalyzeArticle method return the right type
              *  |
@@ -29,36 +30,32 @@ namespace SemanticAnalyzer
              * \ /
              *  V
              */
-            //return MeaningCloud.AnalyzeArticle(articleText);
-            throw new NotImplementedException();
+            //return MeaningCloud.AnalyzeArticle(articleData.Text);
+            throw new AggregateException();
+        }   
+
+
+        private ArticleData GetArticleData(string url)
+        {
+
+            var requestContent = "https://api.diffbot.com/v3/article?" +
+                "token=642d8c5e2e5f11ca47e3128716bea5dc&url=" +
+                url +
+                "&fields=title,text,siteName";
+            var request = (HttpWebRequest)WebRequest.Create(requestContent);
+            var response = (HttpWebResponse)request.GetResponse();
+            var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+            var result = JsonConvert.DeserializeObject<dynamic>(responseString);
+            var objects = result.objects[0];
+            return new ArticleData(objects.title.ToString(), objects.text.ToString(), objects.siteName.ToString());
         }
 
-        private NewsPaper GetNewsPaper()
-        {
-            throw new NotImplementedException();
-        }
-
-        private string GetArticleText(string url)
-        {
-            throw new NotImplementedException();
-        }
     }
 
 
     public class TopicAgenda
     {
         public string Name { get; set; }
-        public int Ogenda  { get; set; }
+        public int Agenda  { get; set; }
     }
-
-
-
-    public enum NewsPaper
-    {
-        NewYorkTimes,
-        Telegraph,
-        Gardian,
-        WallStreetJournal,
-        Other,
-    };
 }
