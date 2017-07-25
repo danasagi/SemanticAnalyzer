af70aabb1e465e6abfb3ccd52ae9bcb41a827547
Oz <ozba@microsoft.com>
2017-07-25 16:05:40 +0300

    subtextServer

---
+++ SubtextServer/SubtextServer/Areas/HelpPage/ModelDescriptions/ParameterAnnotation.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SubtextServer.Areas.HelpPage.ModelDescriptions
+{
+    public class ParameterAnnotation
+    {
+        public Attribute AnnotationAttribute { get; set; }
+
+        public string Documentation { get; set; }
+    }
+}
